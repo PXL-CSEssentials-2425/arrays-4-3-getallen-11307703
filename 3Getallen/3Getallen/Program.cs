f45876fd468ce381c34bc4bd27b545fc9c4f92ad
11307703 <11307703@student.pxl.be>
2024-12-15 19:46:55 +0100

    finaal

---
+++ 3Getallen/3Getallen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace _3Getallen
+{
+    internal class Program
+    {
+        private static int[] numbers = new int[3];
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==Som bepalen van 3 getallen==");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int sum = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Geef getal {i+1}/3 : ");
+                numbers[i] = int.Parse(Console.ReadLine());
+                sum += numbers[i];
+            }
+
+            Console.Write($"De som is {sum}");
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
